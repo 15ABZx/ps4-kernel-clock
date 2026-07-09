73c6a5d3b25fc7397efd5f3e07f720df6571e9c5
15ABZx <155449025+15ABZx@users.noreply.github.com>
2026-07-09 10:33:19 +0200

    Use upstream source tree and FW7 SRTC mode paths

--- source/main.c
+++ source/main.c
@@ -1,33 +1,16 @@
-//#define DEBUG_SOCKET
-#define DEBUG_IP "192.168.2.2"
-#define DEBUG_PORT 9023
-
-#include "ps4.h"
+#include "kernel_utils.h"
 
 int _main(struct thread *td) {
-  UNUSED(td);
-
   initKernel();
   initLibc();
+  initPthread();
 
-#ifdef DEBUG_SOCKET
-  initNetwork();
-  DEBUG_SOCK = SckConnect(DEBUG_IP, DEBUG_PORT);
-#endif
-
-  jailbreak();
-  kernel_clock(14861963);
+  uint64_t fw_version = get_fw_version();
+  jailbreak(fw_version);
 
   initSysUtil();
-  
-  char fw_version[6] = {0};
-  get_firmware_string(fw_version);
-  printf_notification("Kernel time changed!\nPS4 Firmware %s", fw_version);
 
-#ifdef DEBUG_SOCKET
-  printf_debug("Closing socket...\n");
-  SckClose(DEBUG_SOCK);
-#endif
+  printf_notification("Kernel time changed!");
 
   return 0;
 }
